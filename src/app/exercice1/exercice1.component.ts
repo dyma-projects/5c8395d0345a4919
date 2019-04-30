d8cd104401c4dfca6b2549c51a9e8c9f633b3a22
Dikra <elmouttakidikra@gmail.com>
2019-04-30 13:16:11 +0200

    projet2

---
+++ src/app/exercice1/exercice1.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice1',
+  templateUrl: './exercice1.component.html',
+  styleUrls: ['./exercice1.component.css']
+})
+export class Exercice1Component implements OnInit {
+
+  public showMe = "Cliquez ici"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  imgClick() {
+    console.log("Hello World")
+  }
+
+}
--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -12,4 +12,8 @@ export class Exercice2Component implements OnInit {
   ngOnInit() {
   }
 
+  clickMe() {
+    console.log("Click : " );
+  }
+
 }
